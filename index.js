799a5c29666229272ddfbfa6c2a6030c98f55cf7
jpcon1 <josephpauloconcepcion@gmail.com>
2024-02-04 07:15:07 +0800

    add aos library

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const handleScroll = () => {
-    const e = document.getElementById('c2').getBoundingClientRect();
+// const handleScroll = () => {
+//     const e = document.getElementById('c2').getBoundingClientRect();
 
-    if(e.top === 0){
-    document.getElementById('d1').style.setProperty('animation','fadeIn 1s')
-    document.getElementById('d1').style.setProperty("visibility",'visible')
-    }
+//     if(e.top === 0){
+//     document.getElementById('d1').style.setProperty('animation','fadeIn 1s')
+//     document.getElementById('d1').style.setProperty("visibility",'visible')
+    
+//     }
 
     
     
 
 
-}
+// }
 
 
 
-const scroll = document.addEventListener('scroll', handleScroll);
+// const scroll = document.addEventListener('scroll', handleScroll);
 
