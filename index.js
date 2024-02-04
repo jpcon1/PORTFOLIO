28341499f0d307ff139ab2324e23e52f6dbb6156
jpcon1 <josephpauloconcepcion@gmail.com>
2024-02-04 08:53:29 +0800

    add animation library

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
-// const handleScroll = () => {
-//     const e = document.getElementById('c2').getBoundingClientRect();
 
-//     if(e.top === 0){
-//     document.getElementById('d1').style.setProperty('animation','fadeIn 1s')
-//     document.getElementById('d1').style.setProperty("visibility",'visible')
-    
-//     }
-
-    
-    
-
-
-// }
-
-
-
-// const scroll = document.addEventListener('scroll', handleScroll);
 
