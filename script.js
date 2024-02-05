78b92d2833686d83f5dfb136ed9211de0fecb07e
jpcon1 <josephpauloconcepcion@gmail.com>
2024-02-05 15:23:24 +0800

    commit

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 import { gsap } from "./node_modules/gsap/index.js";
-// import { ScrollTrigger } from "./node_modules/gsap/ScrollTrigger.min.js";
-
 
 gsap.registerPlugin(ScrollTrigger);
 const animatef = gsap.from;
@@ -9,19 +7,21 @@ animatef('.navigation-list>li',{duration:1,y:'-100%',stagger:0.05});
 animatef('.box1>h1',{duration:1,x:'-100%',stagger:0.2})
 
 animatef('.skills-container>img',{
-
     rotation:-360,
     duration:1,
     x:'-100%',
     stagger:0.1,
-
+    opacity:0,
+     
     scrollTrigger:{
     trigger:".box1",
     start:"top top",
-    toggleActions:"play"
+    toggleActions: "restart play reverse restart",
+    scrub:1
     },
     
 })
+
 
 
 
